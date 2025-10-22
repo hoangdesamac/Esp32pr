ae04bbb43eaa08e64321b6925a15c6676e85925c
hoangdesamac <tanthat2019@gmail.com>
2025-10-22 21:28:28 +0700

    push code for display in lcd and reading to ldr sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,19 @@
-#include <iostream>
+#include <Arduino.h>
+#include <ESP32Servo.h>
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+const int ldrDO = 27;
+void setup() {
+  Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+}
+
+void loop() {
+  int value = digitalRead(ldrDO);
+  lcd.print("LDR Value:");
+  lcd.setCursor(10, 0);
+  lcd.print(value);
+  delay(500);
+}
