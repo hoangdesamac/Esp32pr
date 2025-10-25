65f4be94f67df100f01ece44ace4c2e8a6db553a
hoangdesamac <tanthat2019@gmail.com>
2025-10-25 23:15:16 +0700

    stimulate rain sensor

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,26 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 const uint8_t LDR_DO = 27;
+const uint8_t RAIN_DO = 14;
 
 void setup() {
     Serial.begin(9600);
     lcd.init();
     lcd.backlight();
-    lcd.setCursor(0,0);
+    lcd.setCursor(0, 0);
     lcd.print("LDR Value:");
-  pinMode_custom(LDR_DO, INPUT); // hoặc INPUT_PULLUP/INPUT_PULLDOWN tùy mạch
+    lcd.setCursor(0, 1);
+    lcd.print("Rain Value: ");
+    pinMode_custom(LDR_DO, INPUT); // hoặc INPUT_PULLUP/INPUT_PULLDOWN tùy mạch
+    pinMode_custom(RAIN_DO, INPUT);
 }
 
 void loop() {
-    int value = digitalRead_custom(LDR_DO);
+    int valueLDR = digitalRead_custom(LDR_DO);
+    int valueRAIN = digitalRead_custom(RAIN_DO);
     lcd.setCursor(11,0);
-    lcd.print(value);
-    lcd.print(' ');
+    lcd.print(valueLDR);
+    lcd.setCursor(11, 1);
+    lcd.print(valueRAIN);
     delay_custom(500);
 }
